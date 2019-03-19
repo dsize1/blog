0dfaf6b7b125051488fe8a0a6127a393f8c4d6a0
zhaocheng <32454198+dsize1@users.noreply.github.com>
2019-03-19 21:51:05 +0800

    push bundle

---
+++ build/precache-manifest.700e091098ed528ca523b319071e758c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e045b5cfab22fc9a4d5",
+    "url": "/static/js/runtime~main.3e045b5c.js"
+  },
+  {
+    "revision": "415359ced1d50f0f086f",
+    "url": "/static/js/main.415359ce.chunk.js"
+  },
+  {
+    "revision": "680704760c0ea806eeba",
+    "url": "/static/js/5.68070476.chunk.js"
+  },
+  {
+    "revision": "94dc22a75563da9d18ef",
+    "url": "/static/js/4.94dc22a7.chunk.js"
+  },
+  {
+    "revision": "00ec5549b7edef8ad4fe",
+    "url": "/static/js/3.00ec5549.chunk.js"
+  },
+  {
+    "revision": "0924bc90f609ed09c760",
+    "url": "/static/js/2.0924bc90.chunk.js"
+  },
+  {
+    "revision": "d53bdff9374416ba5a91",
+    "url": "/static/js/1.d53bdff9.chunk.js"
+  },
+  {
+    "revision": "415359ced1d50f0f086f",
+    "url": "/static/css/main.77a00916.chunk.css"
+  },
+  {
+    "revision": "94dc22a75563da9d18ef",
+    "url": "/static/css/4.1fd218b1.chunk.css"
+  },
+  {
+    "revision": "fa64706eabebcb60908ce2074e930d47",
+    "url": "/index.html"
+  }
+];
